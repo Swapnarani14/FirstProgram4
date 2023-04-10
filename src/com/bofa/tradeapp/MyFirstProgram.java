88997f15e879cd4c0826ed77b1445b0429e3eea7
Swapnarani14 <gandhari.swapna9@gmail.com>
2023-04-10 12:22:58 -0500

    first commit

---
+++ src/com/bofa/tradeapp/MyFirstProgram.java
@@ -0,0 +1,14 @@
+package com.bofa.tradeapp;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is swapna");
+		System.out.println("This is my FirstProgram");
+		System.out.println("Thank you SAANVI");
+		
+
+	}
+
+}
